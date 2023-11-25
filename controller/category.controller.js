2970a1d4c523406e15287f6fa8cbb2ebf0e44686
hvgSudo <harshvardhanguleria@gmail.com>
2023-11-25 13:50:44 +0530

    push

---
+++ controller/category.controller.js
@@ -0,0 +1,40 @@
+import CategoryModel from "../model/category.model.js";
+
+class CategoryController {
+  async getCategories() {
+    const categries = await CategoryModel.find();
+    
+    res.status(200)
+    res.json(categories)
+  }
+
+  async getCategory(req, res) {
+    const data = await CategoryModel.findById(req.params.id);
+    res.status(200)
+    res.json(data)
+  }
+
+  async create(req, res) {
+    const result = await CategoryModel.create(req.body);
+    console.log(result)
+    res.status(200)
+    res.json({ message: 'OK', success: true});
+  }
+
+  async update(req, res) {
+    const id = req.params.id;
+    const data = req.body
+    await CategoryModel.findByIdAndUpdate(id, data);
+    res.status(200)
+    res.json({ message: 'OK', success: true});
+  }
+
+  async delete (req, res) {
+    const id = req.params.id
+    const result = await CategoryModel.findByIdAndDelete(id);
+    res.status(200)
+    res.json({ message: 'Category was deleted successfully!', success: true});
+  }
+}
+
+export default CategoryController;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import userRouter from './user.route.js';
+import categoryRouter from './category.route.js';
 
 const router = express.Router();
 router.use('/user', userRouter);
+router.use('/category', categoryRouter);
 
+export default router;
