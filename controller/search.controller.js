76de715761dec069c8e4e7b5b4efbe56a0713278
hvgSudo <harshvardhanguleria@gmail.com>
2023-11-28 15:14:13 +0530

    Corrected search functionality

--- controller/search.controller.js
+++ controller/search.controller.js
@@ -2,23 +2,11 @@ import ProductModel from "../model/product.model.js";
 
 class SearchController{
     async searchProducts(req, res) {
-        console.log('query ', req.query);
         const search = req.query.search;
-        const products = await ProductModel.find({ name: `/${search}/i` }).exec();
-        console.log('products ', products);
+        const products = await ProductModel.find({ name: {$regex: search, $options: 'i'} });
         res.status(200)
         res.json(products)
       }
 }
 
 export default SearchController;
-
-// exports.search = async (req, res) => {
-//   const { query } = req.query;
-
-//   const products = await Product.find({
-//     name: { $regex: new RegExp(query, 'i') },
-//   });
-
-//   res.json(products);
-// };
