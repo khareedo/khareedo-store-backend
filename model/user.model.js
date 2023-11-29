68b6170be1d54f0759ad0a933fa0f14b59ffc5dd
Govinda Mandal <govinda4india@gmail.com>
2023-11-30 00:51:34 +0530

    Updated user schema

--- model/user.model.js
+++ model/user.model.js
@@ -1,7 +1,7 @@
 import {Schema, model} from 'mongoose';
 
 const userSchema = new Schema({
-  name: String,
+  name: { type: String, required: true },
   username: { type: String, unique: true, required: true },
   gender: String,
   email: { type: String, unique: true, required: true },
